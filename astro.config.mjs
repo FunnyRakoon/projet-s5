6e5aec76b96b235caa5adf5cc44aab86186a4f59
FunnyRakoon <hiwa.pouf049@gmail.com>
2025-09-03 09:38:32 +0200

    page

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://funnyrakoon.github.io',
+  base: '/projet-s5',
   vite: {
     plugins: [tailwindcss()]
   }
